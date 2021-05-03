99e990b793ddc611d67d5217e1122f3ab7119975
João Gomes <jolinharesgomes@gmail.com>
2021-05-03 14:08:25 +0100

    Add views and assets

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('projects');
+});
+Route::get('about', function () {
+    return view('about');
+});
+Route::get('contact', function () {
+    return view('contact');
 });
